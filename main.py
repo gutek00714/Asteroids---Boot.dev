879b732015916e6fe5024f4f7d7be58ad62990db
gutek00714 <guciolaczkowski1@gmail.com>
2025-12-11 19:13:31 +0100

    Finished gameloop part

--- main.py
+++ main.py
@@ -10,24 +10,37 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
 
-    #initialize pygame
+    # Initialize pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    #game loop
+    # Create a clock object to track time
+    clock = pygame.time.Clock()
+
+    # Initialize delta time
+    dt = 0
+
+    # Game loop
     while True:
         log_state()
 
-        #handle quit event
+        # Handle quit event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
 
-        #fill the screen with solid black
+        # Fill the screen with solid black
         screen.fill("black")
 
-        #refresh the screen
+        # Refresh the screen
         pygame.display.flip()
+
+        # Limit the loop to 60 frames per second and calculate delta time
+        # .tick(60) pauses the loop to maintain ~60 FPS
+        # Returns the time elapsed since the last call in milliseconds
+        # Dividing by 1000 converts it to seconds for frame-independent movement
+        dt = clock.tick(60) / 1000
+        #print(dt)
 
 if __name__ == "__main__":
     main()
